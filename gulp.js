734665288887519fbb51ded4827f24f06233aa58
Roman K <41487922+kussch@users.noreply.github.com>
2019-03-27 21:58:40 +0300

    some comments for livereload plagin

--- gulp.js
+++ gulp.js
@@ -1,14 +1,13 @@
-var gulp = require('gulp');
-var livereload = require ('gulp-livereload');
+var gulp = require('gulp'); //here we assign our gulp task-manager in variable
+var livereload = require ('gulp-livereload'); //here we assign livereload method 
 
-gulp.task('livereload', function(){
-
-gulp.src('.css/*.css');
-	.pipe(livereload());
+gulp.task('livereload', function(){    //here we set the task in which 
+	gulp.src('.css/*.css'); //  set indicated files extensions to work with 
+	.pipe(livereload()); // with .pipe gulp method we can chain some multiple tasks together  
 });
 
-gulp.task('default', function(){
-	livereload.listen();
-	gulp.watch('.css/*.css', ['livereload']);
+gulp.task('default', function(){ 
+	livereload.listen(); //starts livereload server 
+	gulp.watch('.css/*.css', ['livereload']); //here we say galp to track over all of the indicated files modificationsm, changes 
 
 });
